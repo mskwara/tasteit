795ecc40df6afa4ac67ba02036dc0e786763f291
Michał Skwara <michalon2000@gmail.com>
2020-09-27 14:17:45 +0200

    serve static files

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const recipeRouter = require("./routes/recipeRouter");
 const userRouter = require("./routes/userRouter");
 const reviewRouter = require("./routes/reviewRouter");
@@ -7,18 +7,21 @@ const cookieParser = require("cookie-parser");
 const globalErrorHandler = require("./controllers/errorController");
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 // Access-Control-Allow-Origin *
 // api.natours.com, front-end natours.com
 // app.use(cors({
 //   origin: 'https://www.natours.com'
 // }))
 
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use(express.json({ limit: "10kb" }));
 
 app.use(cookieParser());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // ROUTES
 app.use("/api/v1/recipes", recipeRouter);
